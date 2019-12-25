8ced66e24bb07558f19a12cfc624bbf0414a9bc2
luji17343080 <1483220884@qq.com>
2019-12-26 00:00:09 +0800

    add .

---
+++ 作业十一：AR、MR技术/UnityProject/AR/Assets/VirtualButtonEventHandler.cs
@@ -0,0 +1,36 @@
+﻿﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class VirtualButtonEventHandler : MonoBehaviour, IVirtualButtonEventHandler
+{
+    // 创建虚拟按钮
+    public GameObject vb;
+    public Animator an;
+    // 按键事件
+    void IVirtualButtonEventHandler.OnButtonPressed(VirtualButtonBehaviour vb)
+    {
+        an.SetBool("Swing", true);
+        Debug.Log("swing!");
+    }
+    
+    void IVirtualButtonEventHandler.OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+        an.SetBool("Swing", false);
+        Debug.Log("static!");
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        VirtualButtonBehaviour flag = vb.GetComponent<VirtualButtonBehaviour>();
+        if(flag)
+        {
+            flag.RegisterEventHandler(this);
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
